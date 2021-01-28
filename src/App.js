af8463608e0c519eb9dc16b382bd673c4d5486f1
dagrel <dagrel@users.noreply.github.com>
2021-01-28 11:10:48 +0100

    dsadsa

--- src/App.js
+++ src/App.js
@@ -9,15 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          yooooooooo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
